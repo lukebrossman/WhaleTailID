3e34d9bd89f001ad22029b4c11a0b8e16a1cc2c8
lbrossma <luke.brossman@wsu.edu>
2019-04-16 17:20:53 -0700

    messing around with image resizing

--- cnn.py
+++ cnn.py
@@ -16,7 +16,10 @@ img_rows, img_cols = 28, 28
 #the data, split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = x_train.reshape(60000,28,28,1)
+print(x_train[0])
+print(y_train)
+
+"""x_train = x_train.reshape(60000,28,28,1)
 x_test = x_test.reshape(10000,28,28,1)
 print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
@@ -43,4 +46,4 @@ model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimi
 model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_test, y_test))
 score = model.evaluate(x_test,y_test, verbose=0)
 print('Test loss:', score[0])
-print('Test Accuracy:', score[1])
+print('Test Accuracy:', score[1])"""
--- whale.py
+++ whale.py
@@ -10,87 +10,36 @@ import sys
 from PIL import Image
 import os
 
-def minpt():
-    batch_size = 128
-    num_classes = 10
-    epochs = 12
-
-    #image dimensions
-    img_rows, img_cols = 28, 28
-
-    #the data, split between train and test sets
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-    x_train = x_train.reshape(60000,28,28,1)
-    x_test = x_test.reshape(10000,28,28,1)
-    print('x_train shape:', x_train.shape)
-    print(x_train.shape[0], 'train samples')
-    print(x_test.shape[0], 'test samples')
-
-    #convert class vectors to binary class matricies
-    y_train = keras.utils.to_categorical(y_train, num_classes)
-    y_test = keras.utils.to_categorical(y_test, num_classes)
-
-    model = Sequential()
-    model.add(Conv2D(32, kernel_size=(3,3), activation='relu', input_shape=(28,28,1)))
-
-    model.add(Conv2D(64,(3,3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
-    model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(num_classes, activation='softmax'))
-
-
-    model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adadelta(), metrics=['accuracy'])
-
-    model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_test, y_test))
-    score = model.evaluate(x_test,y_test, verbose=0)
-    print('Test loss:', score[0])
-    print('Test Accuracy:', score[1])
-
-
-
-pic_arr = []
-cnt = 0
-f = open("./image_data", "a")
-
 def get_arrays(welp):
-
+    pic_arr = []
     os.chdir("./train")
-    #for pic in welp[1:]:
-    image = Image.open(welp[0][0]) 
-    arr = np.array(image)
-    image.close()
-    arr[20,30]
-    print(arr, "end")
-    arr = np.array2string(arr)
-    print(arr,"end")
-        #f.write(np.array2string(arr))
-        #pic_arr.append(arr)
-    f.close()
+    i = 0
+    print("go fuk yourself")
+    for pic in welp[1:]:
+        i += 1
+        image = Image.open(pic[0])
+        image = image.resize((60,60), Image.ANTIALIAS)
+        arr = np.array(image)
+        arr = rgb2gray(arr)
+        image.close()
+        pic_arr.append(arr)
+        if i % 500 == 0:
+            print(i)
+    
     return pic_arr
 
-
-
-
-
-
-
-
-
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
 
 def FileToArray(file):
-    with open(file, 'rU') as f:  
+    with open(file, 'r') as f:  
         reader = csv.reader(f)
-    data = list(list(rec) for rec in csv.reader(f, delimiter=',')) #reads csv into a list of lists
+        data = list(list(rec) for rec in csv.reader(f, delimiter=',')) #reads csv into a list of lists
     return data
 
 def main():
     file = sys.argv[1]
     blerble = FileToArray(file)
-    print(blerble)
     arrays = get_arrays(blerble)
     #print(arrays)
 
